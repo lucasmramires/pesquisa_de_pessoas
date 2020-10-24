6bdaa010f9922c2f9a00c4980572626192034572
Lucas Ramires <lucasm.ramires@hotmail.com>
2020-10-23 22:28:42 -0300

    correction, putting codification  and testing connection

--- php_action/db_connect.php
+++ php_action/db_connect.php
@@ -2,8 +2,14 @@
 //db_connection
 
 $servername = "localhost";
-$usarname = "root";
+$username = "root";
 $password = "";
 $db_name = "lancheria";
 
 $connect = mysqli_connect($servername, $username, $password, $db_name);
+
+mysqli_set_charset($connect, "utf8")
+
+if(mysqli_connect_error()):
+    echo "Erro na conexão: ".mysqli_connect_error();
+endif;
