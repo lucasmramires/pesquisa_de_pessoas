e44a851a45ab59bdced679093d49c97d0a67fe55
Lucas Ramires <lucasm.ramires@hotmail.com>
2020-10-23 22:29:11 -0300

    code fix

--- php_action/db_connect.php
+++ php_action/db_connect.php
@@ -8,7 +8,7 @@ $db_name = "lancheria";
 
 $connect = mysqli_connect($servername, $username, $password, $db_name);
 
-mysqli_set_charset($connect, "utf8")
+mysqli_set_charset($connect, "utf8");
 
 if(mysqli_connect_error()):
     echo "Erro na conexão: ".mysqli_connect_error();
