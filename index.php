dcf0b11182dffbec4c848a24b77f6c3df0c52c3f
Lucas Ramires <lucasm.ramires@hotmail.com>
2020-10-23 22:12:18 -0300

    creating table struture

--- index.php
+++ index.php
@@ -4,6 +4,36 @@
 include_once 'includes/header.php';
 ?>
 
+<div class="row">
+    <div class="col s12 m6 push-m3">
+        <h3>Dados das Pessoas</h3>
+        <table class="striped">
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Altura</th>
+                    <th>Intolerância a Lactose</th>
+                    <th>Peso</th>
+                    <th>Atleta</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>Fulano</td>
+                    <td>1.75</td>
+                    <td>Sim</td>
+                    <td>84.7</td>
+                    <td>Sim</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+
+
+
+
 <?php
 //footer
 include_once 'includes/footer.php';
