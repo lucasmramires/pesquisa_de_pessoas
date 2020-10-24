2a257ecf199ba412b889e406d529286bad5a182a
Lucas Ramires <lucasm.ramires@hotmail.com>
2020-10-23 22:18:44 -0300

    creating db connection

---
+++ php_action/db_connect.php
@@ -0,0 +1,9 @@
+<?php
+//db_connection
+
+$servername = "localhost";
+$usarname = "root";
+$password = "";
+$db_name = "lancheria";
+
+$connect = mysqli_connect($servername, $username, $password, $db_name);
