04121bbcadb04c1a5acd365abb0c2efd86673cdf
Lucas Ramires <lucasm.ramires@hotmail.com>
2020-10-23 22:08:47 -0300

    making header and footer inclusion on index

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Sistema de Visualização de Dados</title>
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    </head>
+<?php
 
-    <body>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    </body>
-</html>
+//header
+include_once 'includes/header.php';
+?>
+
+<?php
+//footer
+include_once 'includes/footer.php';
+?>
+
+
+
