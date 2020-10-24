d5bda23a7d21b98a1a6621ac41ec037a93b50e63
Lucas Ramires <lucasm.ramires@hotmail.com>
2020-10-23 22:01:57 -0300

    header creation
    Putting the head lines in the header file to not repeat the code in the others files

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
-  <html>
+<html>
     <head>
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta charset="utf-8">
+        <title>Sistema de Visualização de Dados</title>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
 
-
-
     <body>
-      <script type="text/javascript" src="js/materialize.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     </body>
-  </html>
+</html>
