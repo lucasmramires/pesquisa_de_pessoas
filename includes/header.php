305e1cd24cddf89d685d48fb964eb388341030a4
Lucas Ramires <lucasm.ramires@hotmail.com>
2020-10-26 11:02:46 -0300

    stylesheet creation and link on header

--- includes/header.php
+++ includes/header.php
@@ -6,6 +6,6 @@
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link rel="stylesheet" type="text/css" href="stylesheet.css" media="screen" />
     </head>
-
-    <body>
+<body>
